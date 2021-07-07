5b95e27778cc92226c6ef552f50435dd7731d19f
Justin Goats <justingoats@yahoo.com>
2021-07-07 01:36:30 -0700

    installed react router and configured webpack.config.js to support react router

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,13 @@ module.exports = {
     // Where files should be sent once they are bundled
     output: {
         path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js'
+        filename: 'index.bundle.js',
     },
     // webpack 5 comes with devServer which loads in development mode
     devServer: {
         port: 3000,
-        watchContentBase: true
+        watchContentBase: true,
+        historyApiFallback: true,
     },
     // Rules of how webpack will take our files, complie & bundle them for the browser 
     module: {
