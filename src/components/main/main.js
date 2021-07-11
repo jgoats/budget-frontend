6a1714ed2be2f62c8397f2d8a46c20b226c72751
Justin Goats <justingoats@yahoo.com>
2021-07-10 20:57:01 -0700

    added main.scss file to the "main" component

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,7 +3,9 @@ import Nav from "../nav/nav.js"
 import LandingPage from "../landingpage/landingpage.js";
 import Registration from "../registration-view/registration-view.js";
 import Login from "../login-view/login-view.js";
+import Footer from "../footer/footer.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./main.scss";
 
 
 export default class Main extends React.Component {
