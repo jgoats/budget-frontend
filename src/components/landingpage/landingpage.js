ebc38f325d07d99492c3d70e2bf27af1a6f757b3
Justin Goats <justingoats@yahoo.com>
2021-07-21 02:25:43 -0700

    added nav component to landingpage

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./landingpage.scss";
 import Graph from "../graph/graph.js";
+import Nav from "../nav/nav.js";
 export default class LandingPage extends React.Component {
     render() {
         return (
-            <div className="main-container">
-                <div className="content-container">
-                    <div className="message">
-                        <div>
-                            <h3 className="heading">A simple and fast way to create a monthly budget.</h3>
-                            <p className="more-information">
-                                Creating a budget can be a pain for a lot of people, including myself. Wouldn't it be nice to have a
-                                simple to use and free budget plan that you can use to meet your financial goals? Well now you can!
-                            </p>
+            <div>
+                <Nav />
+                <div className="main-container">
+                    <div className="content-container">
+                        <div className="message">
+                            <div>
+                                <h3 className="heading">A simple and fast way to create a monthly budget.</h3>
+                                <p className="more-information">
+                                    Creating a budget can be a pain for a lot of people, including myself. Wouldn't it be nice to have a
+                                    simple to use and free budget plan that you can use to meet your financial goals? Well now you can!
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="prototype-graph">
-                        <Graph />
+                        <div className="prototype-graph">
+                            <Graph labels={["food", "gasoline", "rent", "misc", "cloths"]} data={[40, 10, 50, 30, 20]} />
+                        </div>
                     </div>
                 </div>
             </div>
