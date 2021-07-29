e4d410800a1d9f8278b81cd2877be6c0996c33e0
Justin Goats <justingoats@yahoo.com>
2021-07-29 13:39:06 -0700

    modified data for the graph component so that its dynamic

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -6,32 +6,18 @@ export default class Graph extends React.Component {
         super(props);
     }
     render() {
-        const { labels, data } = this.props;
+        const { data, backgroundColor, blackBorder } = this.props;
         return (
             <div>
                 <Doughnut data={{
-                    //labels: ["food", "gasoline", "rent", "misc", "cloths"],
                     labels: [],
                     datasets: [
                         {
                             label: "TeamA Score",
-                            //data: [40, 10, 50, 30, 20],
                             data: data,
-                            backgroundColor: [
-                                "#DEB887",
-                                "#A9A9A9",
-                                "#DC143C",
-                                "#F4A460",
-                                "#2E8B57"
-                            ],
-                            borderColor: [
-                                "#CDA776",
-                                "#989898",
-                                "#CB252B",
-                                "#E39371",
-                                "#1D7A46"
-                            ],
-                            borderWidth: [2, 2, 2, 2, 2]
+                            backgroundColor: backgroundColor,
+                            borderColor: blackBorder,
+                            borderWidth: [1, 1, 1, 1, 1]
                         }
                     ]
                 }} />
