e9b99fee437623065c1fa2045a1b3d2a73883f48
Justin Goats <justingoats@yahoo.com>
2021-07-30 00:41:59 -0700

    now the graph can be dynamically switched from pie to doughnut chart

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -6,21 +6,35 @@ export default class Graph extends React.Component {
         super(props);
     }
     render() {
-        const { data, backgroundColor, blackBorder } = this.props;
+        const { data, backgroundColor, blackBorder, graph } = this.props;
         return (
-            <div>
-                <Doughnut data={{
-                    labels: [],
-                    datasets: [
-                        {
-                            label: "TeamA Score",
-                            data: data,
-                            backgroundColor: backgroundColor,
-                            borderColor: blackBorder,
-                            borderWidth: [1, 1, 1, 1, 1]
-                        }
-                    ]
-                }} />
+            <div> {
+                graph === "Pie" ?
+                    <Pie data={{
+                        labels: [],
+                        datasets: [
+                            {
+                                label: "TeamA Score",
+                                data: data,
+                                backgroundColor: backgroundColor,
+                                borderColor: blackBorder,
+                                borderWidth: [1, 1, 1, 1, 1]
+                            }
+                        ]
+                    }} /> :
+                    <Doughnut data={{
+                        labels: [],
+                        datasets: [
+                            {
+                                label: "TeamA Score",
+                                data: data,
+                                backgroundColor: backgroundColor,
+                                borderColor: blackBorder,
+                                borderWidth: [1, 1, 1, 1, 1]
+                            }
+                        ]
+                    }} />
+            }
             </div>
         )
     }
