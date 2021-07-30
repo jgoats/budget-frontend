ff236c09318447aee2b8c1515cdab15f2d879698
Justin Goats <justingoats@yahoo.com>
2021-07-30 00:42:52 -0700

    updated layout for viewbudget

--- src/components/viewbudget/viewbudget.js
+++ src/components/viewbudget/viewbudget.js
@@ -22,7 +22,9 @@ export default class Viewbudget extends React.Component {
             name: "",
             inputData: [],
             question: "Name for your budget?",
-            requirement: "length must be greater than 2 characters"
+            requirement: "length must be greater than 2 characters",
+            message: "",
+            graph: "Pie"
         }
         this.updateTotal = this.updateTotal.bind(this);
         this.updateCost = this.updateCost.bind(this);
@@ -30,9 +32,21 @@ export default class Viewbudget extends React.Component {
         this.addEnvelope = this.addEnvelope.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
         this.getData = this.getData.bind(this);
+        this.changeToPie = this.changeToPie.bind(this);
+        this.changeToDough = this.changeToDough.bind(this);
         this.total = null;
         this.envelope = null;
         this.cost = null;
+    }
+    changeToDough() {
+        this.setState({
+            graph: "Dough"
+        })
+    }
+    changeToPie() {
+        this.setState({
+            graph: "Pie"
+        })
     }
     updateTotal(e) {
         this.total = e.target.value;
@@ -41,42 +55,60 @@ export default class Viewbudget extends React.Component {
         this.envelope = e.target.value;
     }
     updateCost(e) {
-        this.cost = e.target.value;
-    }
-    addEnvelope() {
-        if (this.state.budgetTotal - this.cost == 0) {
-            this.state.data[0] = this.state.budgetTotal - this.cost;
-            this.setState({
-                envelopes: [...this.state.envelopes, this.envelope],
-                data: [...this.state.data, this.cost],
-                budgetTotal: 0
-            })
-            this.envelope = null;
-            this.cost = null;
-            document.getElementsByClassName("envelope-input")[0].value = "";
-            document.getElementsByClassName("envelope-input")[1].value = "";
+        if (!isNaN(parseInt(e.target.value))) {
+            this.cost = parseInt(e.target.value);
         }
         else {
-            console.log("budget is not 0 yet")
-            let newBorder = 'rgb(0,0,0)';
-            this.state.data[0] = this.state.budgetTotal - this.cost;
+            document.getElementsByClassName("envelope-input")[1].value = "";
+        }
+    }
+    addEnvelope() {
+        if (this.envelope !== "" && !isNaN(parseInt(this.cost))) {
+            if (this.state.budgetTotal - this.cost == 0) {
+                this.state.data[0] = this.state.budgetTotal - this.cost;
+                this.setState({
+                    envelopes: [...this.state.envelopes, this.envelope],
+                    data: [...this.state.data, this.cost],
+                    budgetTotal: 0
+                })
+                this.envelope = null;
+                this.cost = null;
+                document.getElementsByClassName("envelope-input")[0].value = "";
+                document.getElementsByClassName("envelope-input")[1].value = "";
+            }
+            else {
+                console.log("budget is not 0 yet")
+                let newBorder = 'rgb(0,0,0)';
+                this.state.data[0] = this.state.budgetTotal - this.cost;
+                this.setState({
+                    envelopes: [...this.state.envelopes, this.envelope],
+                    data: [...this.state.data, this.cost],
+                    budgetTotal: this.state.budgetTotal - this.cost,
+                    borders: [...this.state.borders, newBorder]
+                })
+                this.envelope = null;
+                this.cost = null;
+                document.getElementsByClassName("envelope-input")[0].value = "";
+                document.getElementsByClassName("envelope-input")[1].value = "";
+            }
+        }
+        else {
             this.setState({
-                envelopes: [...this.state.envelopes, this.envelope],
-                data: [...this.state.data, this.cost],
-                budgetTotal: this.state.budgetTotal - this.cost,
-                borders: [...this.state.borders, newBorder]
+                message: "please fill out both forms correctly"
             })
             this.envelope = null;
             this.cost = null;
-            document.getElementsByClassName("envelope-input")[0].value = "";
-            document.getElementsByClassName("envelope-input")[1].value = "";
+            window.setTimeout(function () {
+                this.setState({
+                    message: ""
+                })
+            }.bind(this), 4000);
         }
-
     }
     handleDelete(e, index) {
         let string = e.target.parentNode.children[0].innerHTML;
-        let arr = string.split("  ");
-        let deletedCost = parseInt(arr[1].replace("$", ""));
+        let arr = string.split(" ");
+        let deletedCost = parseInt(arr[2]);
         let envelopesClone = [...this.state.envelopes];
         let dataClone = [...this.state.data];
         dataClone[0] = dataClone[0] + deletedCost;
@@ -116,7 +148,7 @@ export default class Viewbudget extends React.Component {
     }
     render() {
         const { user, token } = this.props;
-        const { envelopes, budgetTotal, data, colors, name, borders } = this.state;
+        const { envelopes, budgetTotal, data, colors, name, borders, graph } = this.state;
         if (this.state.modelOn) {
             return (
                 <Model question={this.state.question}
@@ -124,33 +156,44 @@ export default class Viewbudget extends React.Component {
             )
         } else {
             return (
-                <div>
+                <div className="edit-budget-main">
                     <UserNav user={user} />
-                    <div className="view-budget-container">
-                        <div className="envelopes">
-                            <div className="form-label-container">
-                                <div>{name}</div>
-                                <div>{budgetTotal}</div>
-                                <label>Add An Envelope</label>
+                    <div className="edit-budget-data-container">
+                        <div className="edit-budget-data">{name} : ${budgetTotal}</div>
+                    </div>
+                    <div className="edit-budget-container">
+                        <div className="form-label-container">
+                            <div>
+                                <label className="model-message">Add An Envelope</label>
                                 <input className="envelope-input" onChange={(e) => this.updateInput(e)} type="text" />
-                                <button onClick={this.addEnvelope}>Add</button>
-                                <label>Add A Price</label>
-                                <input className="envelope-input" onChange={(e) => this.updateCost(e)} type="text" />
+                                <label className="model-message">Envelope Cost</label>
+                                <input className="envelope-input" placeholder="$" onChange={(e) => this.updateCost(e)} type="text" />
+                                <div className="model-message">{this.state.message}</div>
+                                <button className="model-button" onClick={this.addEnvelope}>Add</button>
+                                {
+                                    budgetTotal === 0 ? <div className="edit-budget-submit">Submit Your Budget</div> : <div className="edit-budget-hide"></div>
+                                }
                             </div>
+                        </div>
+
+                        <div className="envelopes">
                             <div className="main-label-container">
                                 {
                                     envelopes.map((item, index) =>
                                         <div style={{ "backgroundColor": `${this.state.colors[index + 1]}` }} key={index} className="label-container">
-                                            <p className="envelope-label">{`${item}  $${data[index + 1]}`}</p>
+                                            <p className="envelope-label">{`${item} ${"$"} ${data[index + 1]}`}</p>
                                             <img onClick={(e) => this.handleDelete(e, index)} className="label-image" src={Bin} />
                                         </div>
                                     )
-
                                 }
                             </div>
                         </div>
-                        <div className="view-budget-graph">
-                            <Graph blackBorder={borders} backgroundColor={colors} data={data} />
+                        <div className="edit-budget-graph">
+                            <div>
+                                <div onClick={this.changeToPie} className="pie-graph">Pie</div>
+                                <div onClick={this.changeToDough} className="doughnut-graph">Doughnut</div>
+                            </div>
+                            <Graph graph={graph} blackBorder={borders} backgroundColor={colors} data={data} />
                         </div>
                     </div>
                 </div>
