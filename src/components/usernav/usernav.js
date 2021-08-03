3ace41363cdc6552c740f83a4e0442ff177c89c3
Justin Goats <justingoats@yahoo.com>
2021-08-03 08:44:14 -0700

    changd the link endpoints to point to the newly created components

--- src/components/usernav/usernav.js
+++ src/components/usernav/usernav.js
@@ -7,12 +7,12 @@ export default function Usernav(props) {
     const { user } = props;
     return (
         <ul className="user-navigation-container">
-            <li className="user-navigation-item"><Link to="/viewbudget">View Budget</Link></li>
-            <li className="user-navigation-item"><Link to="/editbudget">Edit Budget</Link></li>
-            <li className="user-navigation-item"><Link to="/settings" >Settings</Link></li>
+            <li className="user-navigation-item"><Link to="/viewbudgets">View Budgets</Link></li>
+            <li className="user-navigation-item"><Link to="/createbudget">Create Budget</Link></li>
+            <li className="user-navigation-item"><Link to="/deletebudget" >Delete A Budget</Link></li>
             <li className="user-navigation-item"> <div>
                 <img className="logo" src={BarGraph} />
-            </div><div><Link to="/profile">{user}</Link></div></li>
+            </div><div><Link to="/viewbudgets">{user}</Link></div></li>
         </ul>
     )
 }
